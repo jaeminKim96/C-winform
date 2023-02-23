c1a5bf82fc00f05d17e6c278ec10be30f598f1fe
jaeminKim96 <125521550+jaeminKim96@users.noreply.github.com>
2023-02-23 17:59:11 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonSeven = new System.Windows.Forms.Button();
+            this.buttonEight = new System.Windows.Forms.Button();
+            this.buttonNine = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.buttonFour = new System.Windows.Forms.Button();
+            this.buttonFive = new System.Windows.Forms.Button();
+            this.buttonSix = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.buttonOne = new System.Windows.Forms.Button();
+            this.buttonTwo = new System.Windows.Forms.Button();
+            this.buttonThree = new System.Windows.Forms.Button();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonZero = new System.Windows.Forms.Button();
+            this.buttonPoint = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonAdd, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonPoint, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonZero, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonClear, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSub, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonThree, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonTwo, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonOne, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonMul, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSix, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonFive, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonFour, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonDiv, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonNine, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonEight, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSeven, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 55);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(372, 310);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonSeven
+            // 
+            this.buttonSeven.AllowDrop = true;
+            this.buttonSeven.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonSeven.Location = new System.Drawing.Point(3, 3);
+            this.buttonSeven.Name = "buttonSeven";
+            this.buttonSeven.Size = new System.Drawing.Size(87, 71);
+            this.buttonSeven.TabIndex = 0;
+            this.buttonSeven.Text = "7";
+            this.buttonSeven.UseVisualStyleBackColor = true;
+            this.buttonSeven.Click += new System.EventHandler(this.buttonSeven_Click);
+            // 
+            // buttonEight
+            // 
+            this.buttonEight.AllowDrop = true;
+            this.buttonEight.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonEight.Location = new System.Drawing.Point(96, 3);
+            this.buttonEight.Name = "buttonEight";
+            this.buttonEight.Size = new System.Drawing.Size(87, 71);
+            this.buttonEight.TabIndex = 1;
+            this.buttonEight.Text = "8";
+            this.buttonEight.UseVisualStyleBackColor = true;
+            this.buttonEight.Click += new System.EventHandler(this.buttonEight_Click);
+            // 
+            // buttonNine
+            // 
+            this.buttonNine.AllowDrop = true;
+            this.buttonNine.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonNine.Location = new System.Drawing.Point(189, 3);
+            this.buttonNine.Name = "buttonNine";
+            this.buttonNine.Size = new System.Drawing.Size(87, 71);
+            this.buttonNine.TabIndex = 2;
+            this.buttonNine.Text = "9";
+            this.buttonNine.UseVisualStyleBackColor = true;
+            this.buttonNine.Click += new System.EventHandler(this.buttonNine_Click);
+            // 
+            // buttonDiv
+            // 
+            this.buttonDiv.AllowDrop = true;
+            this.buttonDiv.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonDiv.Location = new System.Drawing.Point(282, 3);
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.Size = new System.Drawing.Size(87, 71);
+            this.buttonDiv.TabIndex = 3;
+            this.buttonDiv.Text = "÷";
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            this.buttonDiv.Click += new System.EventHandler(this.buttonDiv_Click);
+            // 
+            // buttonFour
+            // 
+            this.buttonFour.AllowDrop = true;
+            this.buttonFour.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonFour.Location = new System.Drawing.Point(3, 80);
+            this.buttonFour.Name = "buttonFour";
+            this.buttonFour.Size = new System.Drawing.Size(87, 71);
+            this.buttonFour.TabIndex = 4;
+            this.buttonFour.Text = "4";
+            this.buttonFour.UseVisualStyleBackColor = true;
+            this.buttonFour.Click += new System.EventHandler(this.buttonFour_Click);
+            // 
+            // buttonFive
+            // 
+            this.buttonFive.AllowDrop = true;
+            this.buttonFive.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonFive.Location = new System.Drawing.Point(96, 80);
+            this.buttonFive.Name = "buttonFive";
+            this.buttonFive.Size = new System.Drawing.Size(87, 71);
+            this.buttonFive.TabIndex = 5;
+            this.buttonFive.Text = "5";
+            this.buttonFive.UseVisualStyleBackColor = true;
+            this.buttonFive.Click += new System.EventHandler(this.buttonFive_Click);
+            // 
+            // buttonSix
+            // 
+            this.buttonSix.AllowDrop = true;
+            this.buttonSix.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonSix.Location = new System.Drawing.Point(189, 80);
+            this.buttonSix.Name = "buttonSix";
+            this.buttonSix.Size = new System.Drawing.Size(87, 71);
+            this.buttonSix.TabIndex = 6;
+            this.buttonSix.Text = "6";
+            this.buttonSix.UseVisualStyleBackColor = true;
+            this.buttonSix.Click += new System.EventHandler(this.buttonSix_Click);
+            // 
+            // buttonMul
+            // 
+            this.buttonMul.AllowDrop = true;
+            this.buttonMul.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonMul.Location = new System.Drawing.Point(282, 80);
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.Size = new System.Drawing.Size(87, 71);
+            this.buttonMul.TabIndex = 7;
+            this.buttonMul.Text = "X";
+            this.buttonMul.UseVisualStyleBackColor = true;
+            this.buttonMul.Click += new System.EventHandler(this.buttonMul_Click);
+            // 
+            // buttonOne
+            // 
+            this.buttonOne.AllowDrop = true;
+            this.buttonOne.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonOne.Location = new System.Drawing.Point(3, 157);
+            this.buttonOne.Name = "buttonOne";
+            this.buttonOne.Size = new System.Drawing.Size(87, 71);
+            this.buttonOne.TabIndex = 8;
+            this.buttonOne.Text = "1";
+            this.buttonOne.UseVisualStyleBackColor = true;
+            this.buttonOne.Click += new System.EventHandler(this.buttonOne_Click);
+            // 
+            // buttonTwo
+            // 
+            this.buttonTwo.AllowDrop = true;
+            this.buttonTwo.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonTwo.Location = new System.Drawing.Point(96, 157);
+            this.buttonTwo.Name = "buttonTwo";
+            this.buttonTwo.Size = new System.Drawing.Size(87, 71);
+            this.buttonTwo.TabIndex = 9;
+            this.buttonTwo.Text = "2";
+            this.buttonTwo.UseVisualStyleBackColor = true;
+            this.buttonTwo.Click += new System.EventHandler(this.buttonTwo_Click);
+            // 
+            // buttonThree
+            // 
+            this.buttonThree.AllowDrop = true;
+            this.buttonThree.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonThree.Location = new System.Drawing.Point(189, 157);
+            this.buttonThree.Name = "buttonThree";
+            this.buttonThree.Size = new System.Drawing.Size(87, 71);
+            this.buttonThree.TabIndex = 10;
+            this.buttonThree.Text = "3";
+            this.buttonThree.UseVisualStyleBackColor = true;
+            this.buttonThree.Click += new System.EventHandler(this.buttonThree_Click);
+            // 
+            // buttonSub
+            // 
+            this.buttonSub.AllowDrop = true;
+            this.buttonSub.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonSub.Location = new System.Drawing.Point(282, 157);
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.Size = new System.Drawing.Size(87, 71);
+            this.buttonSub.TabIndex = 11;
+            this.buttonSub.Text = "－";
+            this.buttonSub.UseVisualStyleBackColor = true;
+            this.buttonSub.Click += new System.EventHandler(this.buttonSub_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.AllowDrop = true;
+            this.buttonClear.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonClear.Location = new System.Drawing.Point(3, 234);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(87, 73);
+            this.buttonClear.TabIndex = 12;
+            this.buttonClear.Text = "AC";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonZero
+            // 
+            this.buttonZero.AllowDrop = true;
+            this.buttonZero.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonZero.Location = new System.Drawing.Point(96, 234);
+            this.buttonZero.Name = "buttonZero";
+            this.buttonZero.Size = new System.Drawing.Size(87, 73);
+            this.buttonZero.TabIndex = 13;
+            this.buttonZero.Text = "0";
+            this.buttonZero.UseVisualStyleBackColor = true;
+            this.buttonZero.Click += new System.EventHandler(this.buttonZero_Click);
+            // 
+            // buttonPoint
+            // 
+            this.buttonPoint.AllowDrop = true;
+            this.buttonPoint.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonPoint.Location = new System.Drawing.Point(189, 234);
+            this.buttonPoint.Name = "buttonPoint";
+            this.buttonPoint.Size = new System.Drawing.Size(87, 73);
+            this.buttonPoint.TabIndex = 14;
+            this.buttonPoint.Text = ".";
+            this.buttonPoint.UseVisualStyleBackColor = true;
+            this.buttonPoint.Click += new System.EventHandler(this.buttonPoint_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.AllowDrop = true;
+            this.buttonAdd.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonAdd.Location = new System.Drawing.Point(282, 234);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(87, 73);
+            this.buttonAdd.TabIndex = 15;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonResult.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonResult.Location = new System.Drawing.Point(0, 371);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(372, 79);
+            this.buttonResult.TabIndex = 2;
+            this.buttonResult.Text = "=";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 450);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonPoint;
+        private System.Windows.Forms.Button buttonZero;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonSub;
+        private System.Windows.Forms.Button buttonThree;
+        private System.Windows.Forms.Button buttonTwo;
+        private System.Windows.Forms.Button buttonOne;
+        private System.Windows.Forms.Button buttonMul;
+        private System.Windows.Forms.Button buttonSix;
+        private System.Windows.Forms.Button buttonFive;
+        private System.Windows.Forms.Button buttonFour;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Button buttonNine;
+        private System.Windows.Forms.Button buttonEight;
+        private System.Windows.Forms.Button buttonSeven;
+        private System.Windows.Forms.Button buttonResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        enum Operators { None,Add,Substract,Multiply,Divide,Result} //호출
+       
+        Operators currentOperator = Operators.None; //열거형 선언 및 변수 선언 / 데이터 형(int double 등)과 같이 동작
+        //처음은 아무것도 선택 안 되어 있으므로 None
+        Boolean operatorChageFlag = false; //연산자 버튼 눌리면 기존 입력 숫자는 저장/ 새로 받을 숫자에 대한 변수로 사용
+
+        //피연산자
+        double firstOperand;
+        double secondOperand;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOne_Click(object sender, EventArgs e)
+        {
+            // 입력하는 숫자를 구분해야! -> 다 뒤로 덧붙여지고 있음.
+            //연산자 전후를 분리해서 저장해야 하며(연산자 선택되었음을 알 수 있어야 함)
+            // 연산자 누른 후 새로 입력할 숫자를 위해 레이블에 출력된 숫자를 지우는 작업도 필요
+
+            if (operatorChageFlag == true) //연산자가 눌렸는지 눌리면 true임
+            {
+                label1.Text = ""; //기존 문자 삭제
+                operatorChageFlag = false; //다시 지우는 것을 방지하기 위해 false로
+            }
+            string strnumber = label1.Text += "1"; //레이블 초기값이 0인 문자열
+            //01로 저장되어 int.Parse에 의해 1로 변환
+            double intnumber = double.Parse(strnumber); //문자열을 숫자로 변환
+            label1.Text = intnumber.ToString(); //다시 문자열로 변환해서 출력
+        }
+
+        private void buttonTwo_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag==true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "2";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonThree_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag == true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "3";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonFour_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag == true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "4";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonFive_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag == true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "5";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonSix_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag == true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "6";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonSeven_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag == true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "7";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonEight_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag == true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "8";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonNine_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag == true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "9";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonZero_Click(object sender, EventArgs e)
+        {
+            if (operatorChageFlag == true)
+            {
+                label1.Text = "";
+                operatorChageFlag = false;
+            }
+            string strnumber = label1.Text += "0";
+            double intnumber = double.Parse(strnumber);
+            label1.Text = intnumber.ToString();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            firstOperand = 0;
+            secondOperand = 0;
+            currentOperator=Operators.None;
+            label1.Text= "0";
+        }
+
+        private void buttonPoint_Click(object sender, EventArgs e)
+        {
+            label1.Text += ".";
+        }
+
+        private void buttonDiv_Click(object sender, EventArgs e)
+        {
+            firstOperand = double.Parse(label1.Text);
+            currentOperator = Operators.Divide;
+            operatorChageFlag = true;
+        }
+
+        
+        private void buttonMul_Click(object sender, EventArgs e)
+        {
+            firstOperand = double.Parse(label1.Text);
+            currentOperator = Operators.Multiply;
+            operatorChageFlag = true;
+        }
+
+        private void buttonSub_Click(object sender, EventArgs e)
+        {
+            firstOperand=double.Parse(label1.Text);
+            currentOperator = Operators.Substract;
+            operatorChageFlag = true;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            firstOperand = double.Parse(label1.Text); //먼저 입력한 숫자를 정수형으로 바꾼 상태로 저장
+            currentOperator = Operators.Add; //덧셈 버튼이 눌렸음을 알린다.
+            operatorChageFlag= true; //먼저 출력된 값을 지우고 다시 숫자를 출력
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            secondOperand = double.Parse(label1.Text);
+            if(currentOperator== Operators.Add)
+            {
+                firstOperand += secondOperand;
+                label1.Text= firstOperand.ToString();
+            }
+            else if (currentOperator==Operators.Substract)
+            {
+                firstOperand -= secondOperand;
+                label1.Text= firstOperand.ToString();
+            }
+            else if (currentOperator==Operators.Multiply)
+            {
+                firstOperand *= secondOperand;
+                label1.Text= firstOperand.ToString();
+            }
+            else
+            {
+                if (secondOperand==0)
+                {
+                    label1.Text = "ERROR!!";
+                }
+                else
+                {
+                    firstOperand/= secondOperand;
+                    label1.Text= firstOperand.ToString();
+                }
+            }
+        }
+    }
+}
